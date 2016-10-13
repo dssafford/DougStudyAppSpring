2545dafe2c1ae2ba359c311c0d515edf50735b07
Doug Safford <doug.safford@yahoo.com>
2016-10-13 18:28:24 -0400

    added fragments to headers

--- src/main/java/com/doug/DougStudyAppSpringApplication.java
+++ src/main/java/com/doug/DougStudyAppSpringApplication.java
@@ -3,11 +3,6 @@ package com.doug;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import java.text.DateFormat;
-import java.text.SimpleDateFormat;
-import java.util.Calendar;
-import java.util.Date;
-
 
 @SpringBootApplication
 public class DougStudyAppSpringApplication {
@@ -15,14 +10,14 @@ public class DougStudyAppSpringApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(DougStudyAppSpringApplication.class, args);
 
-		DateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
-		//get current date time with Date()
-		Date date = new Date();
-		System.out.println("DATE ====================" + dateFormat.format(date));
-
-		//get current date time with Calendar()
-		Calendar cal = Calendar.getInstance();
-		System.out.println("HEYYYYYY ======================" + dateFormat.format(cal.getTime()));
+//		DateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
+//		//get current date time with Date()
+//		Date date = new Date();
+//		System.out.println("DATE ====================" + dateFormat.format(date));
+//
+//		//get current date time with Calendar()
+//		Calendar cal = Calendar.getInstance();
+//		System.out.println("HEYYYYYY ======================" + dateFormat.format(cal.getTime()));
 
 	}
 
