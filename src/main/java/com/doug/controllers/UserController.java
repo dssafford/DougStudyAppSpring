0fcff5e8717ff16c304dc60dc64c7e94e6ee0a93
Doug Safford <doug.safford@yahoo.com>
2016-11-20 05:04:35 -0600

    user controller fixed now?

--- src/main/java/com/doug/controllers/UserController.java
+++ src/main/java/com/doug/controllers/UserController.java
@@ -1,14 +1,8 @@
 package com.doug.controllers;
 
-<<<<<<< HEAD
-import com.doug.commands.JournalCommand;
-import com.doug.domain.JournalSql;
-import com.doug.services.JournalService;
-=======
 import com.doug.commands.UserCommand;
 import com.doug.domain.User;
 import com.doug.services.UserService;
->>>>>>> users
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -22,63 +16,6 @@ import javax.validation.Valid;
 @Controller
 public class UserController {
 
-<<<<<<< HEAD
-    private JournalService journalService;
-
-    @Autowired
-    public void setJournalService(JournalService journalService) {
-        this.journalService = journalService;
-    }
-
-    @RequestMapping(value = "/journal/list", method = RequestMethod.GET)
-    public String list(Model model){
-        model.addAttribute("journals", journalService.listAllJournals());
-        return "/journal/journals";
-    }
-
-    @RequestMapping("journal/{id}")
-    public String showJournal(@PathVariable Integer id, Model model){
-        model.addAttribute("journal", journalService.getJournalById(id));
-        return "journalshow";
-    }
-
-    @RequestMapping("journal/edit/{id}")
-    public String edit(@PathVariable Integer id, Model model){
-        model.addAttribute("journal", journalService.getJournalById(id));
-        model.addAttribute("journalCommand", new JournalCommand());
-
-        return "journaledit";
-    }
-
-    @RequestMapping("journal/delete/{id}")
-    public String delete(@PathVariable Integer id){
-        journalService.deleteJournal(id);
-
-        return "redirect:/journal/list";
-    }
-
-    @RequestMapping(value = "/journal/new", method = RequestMethod.GET)
-    public String newJournal(Model model){
-
-        model.addAttribute("journal", new JournalSql());
-        model.addAttribute("journalCommand", new JournalCommand());
-
-
-        return "journal/journalformnew";
-    }
-
-    @RequestMapping(value = "/dojournal", method = RequestMethod.POST)
-    public String doJournal(@Valid JournalCommand journalCommand, BindingResult bindingResult,
-                            JournalSql journal){
-
-        if (bindingResult.hasErrors()) {
-            return "journalformnew";
-        }
-
-        JournalSql journalSql = journalService.saveOrUpdateJournal(journal);
-
-        return "redirect:/journal/" + journalSql.getId();
-=======
     private UserService userService;
 
     @Autowired
@@ -134,7 +71,6 @@ public class UserController {
         User UserSql = userService.saveOrUpdateUser(user);
 
         return "redirect:/user/" + user.getId();
->>>>>>> users
 
     }
 
