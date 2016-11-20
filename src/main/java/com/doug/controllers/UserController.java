65a30617bf5026c7c5ba4cce646cc7a885baf708
Doug Safford <doug.safford@yahoo.com>
2016-11-20 05:01:49 -0600

    UserController.java fixed, hopefully

--- src/main/java/com/doug/controllers/UserController.java
+++ src/main/java/com/doug/controllers/UserController.java
@@ -1,8 +1,5 @@
 package com.doug.controllers;
 
-import com.doug.commands.JournalCommand;
-import com.doug.domain.JournalSql;
-import com.doug.services.JournalService;
 import com.doug.commands.UserCommand;
 import com.doug.domain.User;
 import com.doug.services.UserService;
@@ -19,61 +16,62 @@ import javax.validation.Valid;
 @Controller
 public class UserController {
 
-<<<<<<< HEAD
-    private JournalService journalService;
+    private UserService userService;
 
     @Autowired
-    public void setJournalService(JournalService journalService) {
-        this.journalService = journalService;
+    public void setUserService(UserService userService) {
+        this.userService = userService;
     }
 
-    @RequestMapping(value = "/journal/list", method = RequestMethod.GET)
+    @RequestMapping(value = "/user/list", method = RequestMethod.GET)
     public String list(Model model){
-        model.addAttribute("journals", journalService.listAllJournals());
-        return "/journal/journals";
+        model.addAttribute("users", userService.listAllUsers());
+        return "user/users";
     }
 
-    @RequestMapping("journal/{id}")
-    public String showJournal(@PathVariable Integer id, Model model){
-        model.addAttribute("journal", journalService.getJournalById(id));
-        return "journalshow";
+    @RequestMapping("user/{id}")
+    public String showUser(@PathVariable Integer id, Model model){
+        model.addAttribute("User", userService.getById(id));
+        return "usershow";
     }
 
-    @RequestMapping("journal/edit/{id}")
+    @RequestMapping("user/edit/{id}")
     public String edit(@PathVariable Integer id, Model model){
-        model.addAttribute("journal", journalService.getJournalById(id));
-        model.addAttribute("journalCommand", new JournalCommand());
+        model.addAttribute("User", userService.getById(id));
+        model.addAttribute("UserCommand", new UserCommand());
 
-        return "journaledit";
+        return "useredit";
     }
 
-    @RequestMapping("journal/delete/{id}")
+    @RequestMapping("user/delete/{id}")
     public String delete(@PathVariable Integer id){
-        journalService.deleteJournal(id);
+        userService.delete(id);
 
-        return "redirect:/journal/list";
+        return "redirect:/user/list";
     }
 
-    @RequestMapping(value = "/journal/new", method = RequestMethod.GET)
-    public String newJournal(Model model){
+    @RequestMapping(value = "user/new", method = RequestMethod.GET)
+    public String newUser(Model model){
 
-        model.addAttribute("journal", new JournalSql());
-        model.addAttribute("journalCommand", new JournalCommand());
+        model.addAttribute("user", new User());
+        model.addAttribute("userCommand", new UserCommand());
 
 
-        return "journal/journalformnew";
+        return "user/userformnew";
     }
 
-    @RequestMapping(value = "/dojournal", method = RequestMethod.POST)
-    public String doJournal(@Valid JournalCommand journalCommand, BindingResult bindingResult,
-                            JournalSql journal){
+    @RequestMapping(value = "/doUser", method = RequestMethod.POST)
+    public String doUser(@Valid UserCommand UserCommand, BindingResult bindingResult,
+                         User user){
 
         if (bindingResult.hasErrors()) {
-            return "journalformnew";
+            return "userformnew";
         }
 
-        JournalSql journalSql = journalService.saveOrUpdateJournal(journal);
+        User UserSql = userService.saveOrUpdateUser(user);
 
-        return "redirect:/journal/" + journalSql.getId();
+        return "redirect:/user/" + user.getId();
+
+    }
 
 }
